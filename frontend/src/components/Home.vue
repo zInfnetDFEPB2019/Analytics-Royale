de393c29559ad79c8614e1dd25977571b4a63014
Duarte <guilhermeduartt21@gmail.com>
2020-02-28 14:20:48 -0300

    feat: Corpo da home estruturado

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -4,28 +4,32 @@
     <NavBar />
 
     <!-- Corpo da pagina -->
-    <div id="corpo" class="d-flex justify-content-center">
-      <b-col sm="6" class="align-self-center">
-        <b-input-group size="lg" class="mb-3">
-          <b-form-input id="input" placeholder="Procurar..."></b-form-input>
-          <b-input-group-append>
-            <b-form-select
-              size="lg"
-              v-model="selected"
-              :options="options"
-              value-field="value"
-              text-field="text"
-              disabled-field="notEnabled"
-            >
-            </b-form-select>
+    <div class="d-flex justify-content-center">
+      <div id="divImagem">
+        <div id="divInput" class="d-flex align-items-end">
+          <b-input-group size="lg">
+            <b-form-input id="input" placeholder="Procurar..."></b-form-input>
+            <b-input-group-append>
+              <b-form-select
+                size="lg"
+                v-model="selected"
+                :options="options"
+                value-field="value"
+                text-field="text"
+                disabled-field="notEnabled"
+              >
+              </b-form-select>
 
-            <b-button size="lg" variant="warning">
-              <b-icon icon="search" aria-hidden="true"></b-icon>
-            </b-button>
-          </b-input-group-append>
-        </b-input-group>
-      </b-col>
+              <b-button size="lg" variant="warning">
+                <b-icon icon="search" aria-hidden="true"></b-icon>
+              </b-button>
+            </b-input-group-append>
+          </b-input-group>
+        </div>
+      </div>
     </div>
+
+    <!-- Rodape -->
   </div>
 </template>
 
@@ -53,7 +57,25 @@ export default {
 </script>
 
 <style>
-#corpo {
-  height: 85vh;
+#divHome {
+  height: 100vh;
+}
+
+#divImagem {
+  height: 100vh;
+  width: 70%;
+
+  background-image: url(../assets/Rei.png);
+  background-repeat: no-repeat;
+
+  background-position-x: right;
+  background-position-y: center;
+
+  background-size: 40%;
+}
+
+#divInput {
+  height: 66%;
+  width: 83%;
 }
 </style>
