fb298c2b9a35d0337273f2a96382420ca11b905d
Rafael Brito <rafael.souzabrito7@gmail.com>
2020-03-02 19:40:41 -0300

    bug: error de cors

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -7,20 +7,21 @@
       <div id="divImagem">
         <div id="divInput" class="d-flex align-items-end">
           <b-form @submit="getClasherInfo" size="lg">
-            <b-form-input v-model="tag" :id="`type-${type}`" :type="type" placeholder="Procurar..."></b-form-input>
-            <!-- <b-input-group-append> -->
-            <b-form-select
-              size="lg"
-              v-model="selected"
-              :options="options"
-              value-field="value"
-              text-field="text"
-            ></b-form-select>
-
-            <b-button type="submit" size="lg" variant="warning">
-              <b-icon icon="search" aria-hidden="true"></b-icon>
-            </b-button>
-            <!-- </b-input-group-append> -->
+            <b-form-group id="input-group-1">
+              <b-form-input v-model="tag" id="input-1" :type="type" required placeholder="Procurar..."></b-form-input>
+              <b-input-group-append>
+                <b-form-select
+                  size="lg"
+                  v-model="searchOption"
+                  :options="options"
+                  value-field="value"
+                  text-field="text"
+                ></b-form-select>
+                <b-button type="submit" size="lg" variant="warning">
+                  <b-icon icon="search" aria-hidden="true"></b-icon>
+                </b-button>
+              </b-input-group-append>
+            </b-form-group>
           </b-form>
         </div>
       </div>
@@ -32,10 +33,12 @@
 </template>
 
 <script>
-import NavBar from "./NavBar";
-import Footer from "./Footer";
 import axios from "axios";
 require("dotenv").config();
+
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+
 
 export default {
   name: "divHome",
@@ -48,44 +51,47 @@ export default {
   },
   data() {
     return {
-      selected: "1",
+      searchOption: "1",
       options: [
         { value: "1", text: "Jogador" },
         { value: "2", text: "Clã" }
       ],
       tag: "",
-      clasher: ""
     };
   },
-  types: ["tag"],
+  types: ["tag", "searchOption"],
 
   methods: {
     async getClasherInfo(event) {
-      /*eslint-disable no-console*/
-      console.log("chegou no getClasherInfo");
       event.preventDefault();
 
-      let playerTag = this.tag;
+      let searchTag = this.tag;
+      let url = `https://https://api.clashroyale.com/v1/players/${searchTag}`;
+      /*eslint-disable no-console*/
+      console.log(url);
 
-      if (playerTag.includes("#")) {
-        playerTag = playerTag.replace("#", "%23");
+      if (searchTag.includes("#")) {
+        searchTag = searchTag.replace("#", "%23");
       }
-
-      const url = `https://api.clashroyale.com/v1/players/${playerTag}`;
+      
+      if (this.searchOption === "2") {
+        url = url.replace("players", "clans");
+      }
+      
       const option = {
         headers: {
-          Authorization: process.env.API_TOKEN
+          Authorization: process.env.API_TOKEN,
         }
       };
 
       try {
         const result = await axios.get(url, option);
-
         /*eslint-disable no-console*/
         console.log(result);
-        console.log("é isso! try");
+        console.log("SUCCESS!");
       } catch (error) {
-        console.log("é isso! catch");
+        console.log(error);
+        console.log("FAIL!");
       }
     }
   }
